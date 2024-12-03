a7496466f23e6a3e80db4725d6139178bd1836b3
Your Name <you@example.com>
2024-12-03 17:18:52 +0500

    new lesson

---
+++ 3-lesson/lesson.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // string ism,familiya;
+    // cout << "ismingiz nima";
+    // cin >> ism;
+    // cout << "" << ism << "jon familyangiz nima >";
+
+
+    // int yosh,sinf;
+    // float vazn;
+
+    // cout << "" << familiya << "yoshingiz nechada";
+    // cin >> yosh;
+    
+
+    // cout << "" << ism << "vazningiz qancha";
+    // cin >> vazn;
+
+
+ 
+    // cout << endl << " " << ism << "siz " << 12-sinf << " yil oqishingiz kk" << endl;
+    // cout << " " << ism << "siz" << 2024 - yosh << "  -yilda tugilgansiz" << endl;
+    // cout << "" << ism << "siz" <<vazn *1000 << " gramm siz" <<endl;
+    int a=1,b=3;
+
+    int s = a+b;
+
+    cout <<s;
+
+    return 0; 
+}
