08329e564c0a5bce9cbf550d48ce74fa529c8c88
Your Name <you@example.com>
2024-12-04 23:51:08 +0500

    hello

---
+++ 3-lesson/homework.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(){
+    // 1   mashq
+    int a,b,s;
+    cout << "birinchi sonni kiriting:";
+    cin>> a;
+    cout << "ikkinchi sonni kiriting:";
+    cin >>b;
+
+    s = a+b;
+
+    cout << 'sonlarning yigindisi:'<< s << endl;
+
+    // 2   mashq
+    int Perimetr;    
+    cout<< "Perimetrini bilmoqchi bolgan,kvadratingizni bir tomonini kiriting:";
+    cin >> a;
+    Perimetr = 4 *a;
+
+    cout<<"Kvadratning perimetri:" <<Perimetr <<endl;
+
+    
+
+
+
+
+
+    // 3   mashq
+    //int kvadratning yuzasi;
+
+
+
+
+
+
+
+
+
+    // 4   mashq
+    // 5   mashq
+    // 6   mashq
+    // 7   mashq
+    // 8   mashq
+    // 9   mashq
+    // 10  mashq
+    // 11  mashq
+    // 12  mashq
+    // 13  mashq
+    // 14  mashq
+    // 15  mashq
+    // 16  mashq
+    // 17  mashq
+    // 18  mashq
+    // 19  mashq
+    // 20  mashq
+
+
+
+    
+
+    return 0;
+}
